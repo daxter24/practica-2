231cf6059d04ede88b451b620390bfd28f270c89
HP <HP@DESKTOP-KO32DFN>
2021-03-04 22:38:32 -0600

    argumentos

---
+++ Problema2P3/src/Problema2P3/Fruta.java
@@ -0,0 +1,210 @@
+/*******************************************************************************
+ * 2021, All rights reserved.
+ *******************************************************************************/
+package Problema2P3;
+
+import Void;
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of Fruta.
+ * 
+ * @author HP
+ */
+public class Fruta {
+	/**
+	 * Description of the property nombre.
+	 */
+	private String nombre = "";
+	
+	/**
+	 * Description of the property extension .
+	 */
+	private float extension  = 0F;
+	
+	/**
+	 * Description of the property tiempoCosecha.
+	 */
+	private String tiempoCosecha = "";
+	
+	/**
+	 * Description of the property cantCosechaxTiempo.
+	 */
+	private float cantCosechaxTiempo = 0F;
+	
+	/**
+	 * Description of the property costoPromedio.
+	 */
+	private float costoPromedio = 0F;
+	
+	/**
+	 * Description of the property precioVentaProm.
+	 */
+	private float precioVentaProm = 0F;
+	
+	// Start of user code (user defined attributes for Fruta)
+	
+	// End of user code
+	
+	/**
+	 * The constructor.
+	 */
+	public Fruta() {
+		// Start of user code constructor for Fruta)
+		super();
+		// End of user code
+	}
+	
+	/**
+	 * Description of the method AgregarPeriodo.
+	 * @param periodo 
+	 * @param cantEstimada 
+	 * @return 
+	 */
+	public V AgregarPeriodo(String periodo, float cantEstimada) {
+		// Start of user code for method AgregarPeriodo
+		V AgregarPeriodo = null;
+		return AgregarPeriodo;
+		// End of user code
+	}
+	 
+	/**
+	 * Description of the method eliminarPeriodo.
+	 * @param i 
+	 * @return 
+	 */
+	public boolean eliminarPeriodo(int i) {
+		// Start of user code for method eliminarPeriodo
+		boolean eliminarPeriodo = false;
+		return eliminarPeriodo;
+		// End of user code
+	}
+	 
+	/**
+	 * Description of the method costoPeriodo.
+	 * @param i 
+	 * @return 
+	 */
+	public float costoPeriodo(int i) {
+		// Start of user code for method costoPeriodo
+		float costoPeriodo = 0F;
+		return costoPeriodo;
+		// End of user code
+	}
+	 
+	/**
+	 * Description of the method ganancia.
+	 * @param i 
+	 * @return 
+	 */
+	public float ganancia(int i) {
+		// Start of user code for method ganancia
+		float ganancia = 0F;
+		return ganancia;
+		// End of user code
+	}
+	 
+	// Start of user code (user defined methods for Fruta)
+	
+	// End of user code
+	/**
+	 * Returns nombre.
+	 * @return nombre 
+	 */
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	/**
+	 * Sets a value to attribute nombre. 
+	 * @param newNombre 
+	 */
+	public void setNombre(String newNombre) {
+	    this.nombre = newNombre;
+	}
+
+	/**
+	 * Returns extension .
+	 * @return extension  
+	 */
+	public float getExtension () {
+		return this.extension ;
+	}
+	
+	/**
+	 * Sets a value to attribute extension . 
+	 * @param newExtension  
+	 */
+	public void setExtension (float newExtension ) {
+	    this.extension  = newExtension ;
+	}
+
+	/**
+	 * Returns tiempoCosecha.
+	 * @return tiempoCosecha 
+	 */
+	public String getTiempoCosecha() {
+		return this.tiempoCosecha;
+	}
+	
+	/**
+	 * Sets a value to attribute tiempoCosecha. 
+	 * @param newTiempoCosecha 
+	 */
+	public void setTiempoCosecha(String newTiempoCosecha) {
+	    this.tiempoCosecha = newTiempoCosecha;
+	}
+
+	/**
+	 * Returns cantCosechaxTiempo.
+	 * @return cantCosechaxTiempo 
+	 */
+	public float getCantCosechaxTiempo() {
+		return this.cantCosechaxTiempo;
+	}
+	
+	/**
+	 * Sets a value to attribute cantCosechaxTiempo. 
+	 * @param newCantCosechaxTiempo 
+	 */
+	public void setCantCosechaxTiempo(float newCantCosechaxTiempo) {
+	    this.cantCosechaxTiempo = newCantCosechaxTiempo;
+	}
+
+	/**
+	 * Returns costoPromedio.
+	 * @return costoPromedio 
+	 */
+	public float getCostoPromedio() {
+		return this.costoPromedio;
+	}
+	
+	/**
+	 * Sets a value to attribute costoPromedio. 
+	 * @param newCostoPromedio 
+	 */
+	public void setCostoPromedio(float newCostoPromedio) {
+	    this.costoPromedio = newCostoPromedio;
+	}
+
+	/**
+	 * Returns precioVentaProm.
+	 * @return precioVentaProm 
+	 */
+	public float getPrecioVentaProm() {
+		return this.precioVentaProm;
+	}
+	
+	/**
+	 * Sets a value to attribute precioVentaProm. 
+	 * @param newPrecioVentaProm 
+	 */
+	public void setPrecioVentaProm(float newPrecioVentaProm) {
+	    this.precioVentaProm = newPrecioVentaProm;
+	}
+
+
+
+}
